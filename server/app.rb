eeb4181479f4eab31193fe658a0253d15dacc6ff
kawatani hideto <hidemaruhidekyun@gmail.com>
2019-10-27 14:19:09 +0900

    modify logic

--- server/app.rb
+++ server/app.rb
@@ -9,6 +9,7 @@ require './models/index.rb'
 require './utils/CloudinaryHelper.rb'
 
 require 'sinatra/cross_origin'
+require 'securerandom'
 
 Bundler.require
 Dotenv.load
@@ -21,24 +22,42 @@ before do
   response.headers['Access-Control-Allow-Origin'] = '*'
 end
 
-def make_qr(url='https://twitter.com/niza_lit')
-  RQRCode::QRCode.new(url, size: 5, level: :h).as_svg
+def uniq_file_name
+  "#{SecureRandom.hex}.png"
+end
+
+def make_qr(url='https://twitter.com/niza_lit', user)
+  qr = RQRCode::QRCode.new(url, size: 7, level: :h).as_png
+  file = qr.save("tmp/#{uniq_file_name}")
+  return file
 end
 
 post '/api/v1/users/new' do
   baseUrl = 'https://becky-484f7.firebaseapp.com/profile/'
   image_url = CloudinaryHelper.upload(params[:file][:tempfile])
+  puts params
   @user = User.new(name: params[:name], nick_name: params[:nick_name], img: image_url, comment: params[:comment])
   if @user.save
-    url = baseUrl + @user.id
+    url = baseUrl + @user.id.to_s
     qr = make_qr(url)
-    @user.update(qr: qr)
-    params[:links].each do |link|
-      @user.link.create(media_platform_id: link.id, url: link.url)
+    qr_url = CloudinaryHelper.upload(qr.path)
+    File.delete(qr.path)
+    @user.update(qr: qr_url)
+    if params[:links].nil?
+      links = [
+        {id: 1 , url: 'https://twitter.com/niza_lit'},
+        {id: 2 , url: 'https://twitter.com/niza_lit'},
+        {id: 3 , url: 'https://twitter.com/niza_lit'}
+      ]
+    else
+      links = params[:links]
+    end
+    links.each do |link|
+      @user.links.create(media_platform_id: link[:id], url: link[:url])
     end
   end
+  content_type :json
   @user.to_json(include: [{links: {include: :media_platform}}])
-  user.to_json
 end
 
 # ユーザー情報取得
@@ -54,9 +73,20 @@ get '/api/v1/medias' do
   medias.to_json
 end
 
+# get '/api/v1/users/:id/qr' do
+#   baseUrl = 'https://becky-484f7.firebaseapp.com/profile/'
+#   @user = User.find_by_id(params[:id])
+#   content_type :json
+#   @user.qr.to_json
+# end
+
 options "*" do
   response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
   response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
   response.headers["Access-Control-Allow-Origin"] = "*"
   200
+end
+
+get '/' do
+  erb :index
 end
--- server/lib/tasks/seed.rake
+++ server/lib/tasks/seed.rake
@@ -15,12 +15,27 @@ namespace :seed do
 
   task :user do
     url = 'https://becky-484f7.firebaseapp.com/profile/1'
-    User.create(
+    user = User.create(
       name: "川谷英斗",
       nick_name: "ニーザ",
       img: "https://res.cloudinary.com/dlmbukavs/image/upload/v1569656426/aox4qy3ofbnjbxcitwo6.jpg",
-      qr: RQRCode::QRCode.new(url, size: 7, level: :h).as_svg,
+      qr: 'http://res.cloudinary.com/dxladveje/image/upload/v1572146603/ijmfxzrxhjj3anwop99q.png',
       comment: '特技は舌が鼻につくことです'
+    )
+    Link.create(
+      user_id: user.id,
+      media_platform_id: 1,
+      url: "https://twitter.com/niza_lit"
+    )
+    Link.create(
+      user_id: user.id,
+      media_platform_id: 2,
+      url: "https://www.instagram.com/hidetoooooooo/"
+    )
+    Link.create(
+      user_id: user.id,
+      media_platform_id: 3,
+      url: "https://www.facebook.com/kawatani.hideto"
     )
   end
 
